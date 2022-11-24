b0d0c8e676d61844ff56107fc815825d4c294ed2
salmatonka <108433658+salmatonka@users.noreply.github.com>
2022-11-24 23:27:24 +0600

    tenth commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <h1>head</h1>
+    <div>
+      <h1>hhh</h1>
     </div>
   );
-}
+};
 
 export default App;
