30575db9dec33889abc5f3bb7c3c45a3815d2fd4
salmatonka <108433658+salmatonka@users.noreply.github.com>
2022-11-24 14:00:44 +0600

    third commit

--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -14,7 +14,7 @@ const  router = createBrowserRouter([
         errorElement:<ErrorPage></ErrorPage>,
         children:[
             {
-                path:'/',
+                path:'/home',
                 element:<Home></Home>,  
             },
             {
